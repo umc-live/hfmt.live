953009aef1130a62b1f68c6b67dcc5c94e41088c
ramagottfried <rama.gottfried@gmail.com>
2020-04-12 22:40:29 +0200

    reorganizing

---
+++ public/js/webrtc_test.js
@@ -0,0 +1,94 @@
+
+
+const mediaStreamConstraints = {
+    video: true,
+};
+
+// Set up to exchange only video.
+const offerOptions = {
+    offerToReceiveVideo: 1,
+};
+
+// Define initial start time of the call (defined as connection between peers).
+let startTime = null;
+
+// Define peer connections, streams and video elements.
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+
+let localStream;
+let remoteStream;
+
+let localPeerConnection;
+let remotePeerConnection;
+
+
+// Define action buttons. 
+const startButton = document.getElementById('startButton');
+const callButton = document.getElementById('callButton');
+const hangupButton = document.getElementById('hangupButton');
+
+// Set up initial action buttons status: disable call and hangup.
+callButton.disabled = true;
+hangupButton.disabled = true;
+
+
+// Handles start button action: creates local MediaStream.
+function startAction() {
+    startButton.disabled = true;
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+
+    console.log('Requesting local stream.');
+}
+
+
+// Define MediaStreams callbacks.
+
+// Sets the MediaStream as the video element src.
+function gotLocalMediaStream(mediaStream) {
+    localVideo.srcObject = mediaStream;
+    localStream = mediaStream;
+    trace('Received local stream.');
+    callButton.disabled = false;  // Enable call button.
+}
+
+// Handles error by logging a message to the console.
+function handleLocalMediaStreamError(error) {
+    trace(`navigator.getUserMedia error: ${error.toString()}.`);
+}
+
+// Handles remote MediaStream success by adding it as the remoteVideo src.
+function gotRemoteMediaStream(event) {
+    const mediaStream = event.stream;
+    remoteVideo.srcObject = mediaStream;
+    remoteStream = mediaStream;
+    trace('Remote peer connection received remote stream.');
+}
+
+
+// Add behavior for video streams.
+
+// Logs a message with the id and size of a video element.
+function logVideoLoaded(event) {
+    const video = event.target;
+    trace(`${video.id} videoWidth: ${video.videoWidth}px, ` +
+        `videoHeight: ${video.videoHeight}px.`);
+}
+
+// Logs a message with the id and size of a video element.
+// This event is fired when video begins streaming.
+function logResizedVideo(event) {
+    logVideoLoaded(event);
+
+    if (startTime) {
+        const elapsedTime = window.performance.now() - startTime;
+        startTime = null;
+        trace(`Setup time: ${elapsedTime.toFixed(3)}ms.`);
+    }
+}
+
+localVideo.addEventListener('loadedmetadata', logVideoLoaded);
+remoteVideo.addEventListener('loadedmetadata', logVideoLoaded);
+remoteVideo.addEventListener('onresize', logResizedVideo);
+
