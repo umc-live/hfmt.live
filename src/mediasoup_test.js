2b048f44d7c37f22cffa6f7b42b92bf968891213
ramagottfried <rama.gottfried@gmail.com>
2020-04-18 12:46:24 +0000

    minor

--- src/mediasoup_test.js
+++ src/mediasoup_test.js
@@ -12,7 +12,7 @@ async function loadDevice(routerRtpCapabilities)
 {
     try 
     {
-      device = new mediasoup.Device();
+      device = new mediasoupClient.Device();
     } 
     catch (error) 
     {
