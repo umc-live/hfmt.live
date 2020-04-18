f68a6c51011c1d714e365f583151cf4bcffbfcf2
ramagottfried <rama.gottfried@gmail.com>
2020-04-18 19:18:31 +0000

    pre merge

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ async function runMediasoupWorker()
   });
 
   mediasoupRouter = await worker.createRouter( soupconfig.router.mediaCodecs );
+  console.log(mediasoupRouter, soupconfig.router.mediaCodecs );
+
 }
 
 
