9199c05deef17be5ba015e7bb265d662d114b714
ramagottfried <rama.gottfried@gmail.com>
2020-04-27 23:38:40 +0200

    oscilloscope draw

--- src/soupclient.js
+++ src/soupclient.js
@@ -25,7 +25,17 @@ const socket = io()
 
 let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 let analyser = audioCtx.createAnalyser();
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
 let localAudioSource;
+
+// Get a canvas defined with ID "oscilloscope"
+var canvas = document.getElementById("oscilloscope");
+var canvasCtx = canvas.getContext("2d");
+
+
 
 let socketID;
 
@@ -335,7 +345,6 @@ async function sendCameraStreams()
     // add visualizer here for local audio:
     localAudioSource = audioCtx.createMediaStreamSource(localMediaStream);
     localAudioSource.connect(analyser);
-
 
     $('#btn_start').disabled = true;
 
@@ -724,3 +733,42 @@ window.addEventListener('load', () => {
     window.addEventListener('unload', leaveRoom);
 })
 
+
+
+
+function draw() {
+
+  requestAnimationFrame(draw);
+
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvasCtx.fillStyle = "rgb(200, 200, 200)";
+  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+  canvasCtx.beginPath();
+
+  var sliceWidth = canvas.width * 1.0 / bufferLength;
+  var x = 0;
+
+  for (var i = 0; i < bufferLength; i++) {
+
+    var v = dataArray[i] / 128.0;
+    var y = v * canvas.height / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  canvasCtx.lineTo(canvas.width, canvas.height / 2);
+  canvasCtx.stroke();
+}
+
+draw();
