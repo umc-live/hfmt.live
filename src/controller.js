e96bcfbfc51c9423a5439e83484c00e9f2544d9a
ramagottfried <rama.gottfried@gmail.com>
2020-05-08 09:37:57 +0200

    score view dev

--- src/controller.js
+++ src/controller.js
@@ -50,7 +50,7 @@ function processFile(file)
         menu.appendChild(el);
     });
 
-    meun.addEventListener('change', (event) => {
+    menu.addEventListener('change', (event) => {
         console.log('loading event.target.value');
         
         drawsocket.input(obj[event.target.value]);
