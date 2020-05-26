a43dc541f0a4dc684a1947645c51a793ec588258
ramagottfried <rama.gottfried@gmail.com>
2020-05-26 15:25:25 +0200

    added insert html function

--- src/controller.js
+++ src/controller.js
@@ -80,6 +80,30 @@ function arrayBufferToString(file)
 
 }
 
+function insertHTML(html, dest, append=false){
+    // if no append is requested, clear the target element
+    if(!append) dest.innerHTML = '';
+    // create a temporary container and insert provided HTML code
+    let container = document.createElement('div');
+    container.innerHTML = html;
+    // cache a reference to all the scripts in the container
+    let scripts = container.querySelectorAll('script');
+    // get all child elements and clone them in the target element
+    let nodes = container.childNodes;
+    for( let i=0; i< nodes.length; i++) dest.appendChild( nodes[i].cloneNode(true) );
+    // force the found scripts to execute...
+    for( let i=0; i< scripts.length; i++){
+        let script = document.createElement('script');
+        script.type = scripts[i].type || 'text/javascript';
+        if( scripts[i].hasAttribute('src') ) script.src = scripts[i].src;
+        script.innerHTML = scripts[i].innerHTML;
+        document.head.appendChild(script);
+        document.head.removeChild(script);
+    }
+    // done!
+    return true;
+}
+
 function processFile(name, obj, type)
 {
    // let obj = fileToObj(file);
@@ -121,7 +145,8 @@ function processFile(name, obj, type)
         menu.addEventListener('change', (event) => {            
             if( type == 'text/html')
             {
-                $('#main-html').innerHTML = obj;
+               
+                insertHTML(obj, $('#main-html') );
                 testCallMe();
             }
             else if( type == 'text/js' )
