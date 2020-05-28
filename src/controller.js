4cd4909aa96cd4c119efbbdaa780d32473406572
ramagottfried <rama.gottfried@gmail.com>
2020-05-28 22:58:59 +0200

    debugging script loading

--- src/controller.js
+++ src/controller.js
@@ -90,8 +90,13 @@ function arrayBufferToString(file)
 
 async function loadScript(node, src){
     return new Promise((resolve, reject) => {
-        node.onload = () => resolve(true)
+        node.onload = ()=> {
+            document.head.removeChild(script);
+            resolve()
+        };
+        script.onerror = reject;
         node.src = src
+        document.head.appendChild(node);
     })
   }
 
@@ -127,12 +132,18 @@ async function insertHTML(html, dest, append=false){
             //script.src = scripts[i].src;
         }
         else
+        {
             console.log('loading script', i);
+
+            script.innerHTML = scripts[i].innerHTML;
+
+            document.head.appendChild(script);
+
+            document.head.removeChild(script);
+        }
+            
             
 
-        script.innerHTML = scripts[i].innerHTML;
-        document.head.appendChild(script);
-        document.head.removeChild(script);
     }
     // done!
     return true;
